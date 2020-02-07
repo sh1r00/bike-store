1ad7988be21adf2301fac2746d3d81c034bc1d1d
sh1r00 <rasisg@gmail.com>
2020-02-06 16:07:17 -0800

    Updated server host to include netlify env

--- server/api.vue
+++ server/api.vue
@@ -1,13 +1,6 @@
 <script>
+import axios from 'axios'
 export default {
-    methods: {
-        loadData({ commit }) {
-            axios.get('/storedata').then(response => {
-                // console.log(response.data, this)
-                commit('updateStoredata', response.data)
-            })
-        }
-    },
   mounted() {
     if (process.browser) {
       const domElement = document.createElement('script')
@@ -18,22 +11,31 @@ export default {
       document.body.appendChild(domElement)
     }
   },
-    created() {
-        // console.log(this.$store)
-        this.$store.dispatch('loadData')
-    },
-        submitReview(data) {
-          const fs = require('fs')
-          const fileName = require('../static/storedata.json')
-          const file = fileName
+  created() {
+    // console.log(this.$store)
+    this.$store.dispatch('loadData')
+  },
+  methods: {
+    loadData({ commit }) {
+      axios.get('/storedata').then(response => {
+        // console.log(response.data, this)
+        commit('updateStoredata', response.data)
+      })
+    }
+  },
+  submitReview(data) {
+    const fs = require('fs')
+    const fileName = require('../static/storedata.json')
+    const file = fileName
 
-          file.key = data.id
+    file.key = data.id
 
-          fs.writeFile(fileName, JSON.stringify(file, null, 2), function(err) {
-            if (err) return console.log(err)
-            console.log(JSON.stringify(file))
-            console.log('writing to ' + fileName)
-          })
-        }
+    fs.writeFile(fileName, JSON.stringify(file, null, 2), function(err) {
+      // eslint-disable-next-line
+      if (err) return console.log(err)
+      // console.log(JSON.stringify(file))
+      // console.log('writing to ' + fileName)
+    })
+  }
 }
 </script>
